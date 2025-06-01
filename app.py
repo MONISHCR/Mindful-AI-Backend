100a1dc0b8fbc4dcf10eafc1492cc95d85ff5fe9
Monish <monish21052004@gmail.com>
2025-06-01 20:46:10 +0530

    cors

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 hf_api_key = os.getenv("HF_API_KEY")
 
 app = Flask(__name__)
-# CORS(app, origins=["http://localhost:3000"], supports_credentials=True)
-CORS(app, origins="*")
+CORS(app, origins=["https://mindwellness-site.vercel.app"], supports_credentials=True)
+
 
 
 # models = list(genai.list_models())
