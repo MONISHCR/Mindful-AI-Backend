c6e2ad746315e588abbfc61d78ded689e854e5b7
Monish <monish21052004@gmail.com>
2025-06-01 18:26:01 +0530

    updated app.py

--- app.py
+++ app.py
@@ -16,7 +16,9 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 hf_api_key = os.getenv("HF_API_KEY")
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:3000"], supports_credentials=True)
+# CORS(app, origins=["http://localhost:3000"], supports_credentials=True)
+CORS(app, origins="*")
+
 
 # models = list(genai.list_models())
 # print("Available Models:", models)
