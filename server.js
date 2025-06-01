82f83d1ac999098dee4fdbe75e00a323654df11c
Monish <monish21052004@gmail.com>
2025-06-01 18:49:55 +0530

    server

--- server.js
+++ server.js
@@ -43,8 +43,7 @@ const authenticateToken = (req, res, next) => {
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+
 })
 .then(() => console.log("Mongodb connected"))
 .catch((err) => console.log("MongoDB connection error:", err));
