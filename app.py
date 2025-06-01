4f78c3935a9fc9cd26a8e4583cea0c56e3c12ff0
Monish <monish21052004@gmail.com>
2025-06-01 20:27:19 +0530

    upd

--- app.py
+++ app.py
@@ -302,6 +302,7 @@ def analyze_report():
     except Exception as e:
         return jsonify({"error": "Failed to process analysis", "details": str(e)}), 500
     
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))  # Use PORT from environment or default to 5000
-    app.run(host="0.0.0.0", port=port, debug=True)
+# if __name__ == "__main__":
+#     port = int(os.getenv("PORT", 5000))  # Use PORT from environment or default to 5000
+#     app.run(host="0.0.0.0", port=port, debug=True)
+#mm
